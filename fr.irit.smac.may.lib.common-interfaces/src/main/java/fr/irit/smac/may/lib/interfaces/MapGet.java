51d44c4fdc25c379869033f02f43ed5ea183981c
Victor Noël <victor.noel@irit.fr>
2011-08-29 10:47:53 +0200

    forgot an interface

--- fr.irit.smac.may.lib.common-interfaces/src/main/java/fr/irit/smac/may/lib/interfaces/MapGet.java
+++ fr.irit.smac.may.lib.common-interfaces/src/main/java/fr/irit/smac/may/lib/interfaces/MapGet.java
@@ -1,5 +1,9 @@
 package fr.irit.smac.may.lib.interfaces;
 
+/***
+ * Kind of Function<K,V> (fj.F from functional java, or scala.Function1 from Scala)
+ * 
+ */
 public interface MapGet<K,V> {
 
 	public V get(K k);
