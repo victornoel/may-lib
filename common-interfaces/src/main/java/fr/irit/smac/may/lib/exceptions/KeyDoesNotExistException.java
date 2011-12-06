f5a03dc962f2fdb74e1ca2af37551a202ba827e4
Victor Noël <victor.noel@irit.fr>
2011-12-06 17:27:42 +0100

    improved MapGet

---
+++ common-interfaces/src/main/java/fr/irit/smac/may/lib/exceptions/KeyDoesNotExistException.java
@@ -0,0 +1,7 @@
+package fr.irit.smac.may.lib.exceptions;
+
+public class KeyDoesNotExistException extends Exception {
+
+	private static final long serialVersionUID = -4774666953012527693L;
+
+}
--- common-interfaces/src/main/java/fr/irit/smac/may/lib/interfaces/MapGet.java
+++ common-interfaces/src/main/java/fr/irit/smac/may/lib/interfaces/MapGet.java
@@ -1,10 +1,12 @@
 package fr.irit.smac.may.lib.interfaces;
 
+import fr.irit.smac.may.lib.exceptions.KeyDoesNotExistException;
+
 /***
- * Kind of Function<K,V> (fj.F from functional java, or scala.Function1 from Scala)
+ * Kind of Function<K,V> (fj.F from functional java, or scala.Function1 from Scala) but with an exception!
  * 
  */
 public interface MapGet<K,V> {
 
-	public V get(K k);
+	public V get(K k) throws KeyDoesNotExistException;
 }
