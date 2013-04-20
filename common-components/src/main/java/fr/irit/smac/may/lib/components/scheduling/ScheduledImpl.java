8492dec65e18cc2cebf40c38e91dab3a4dcef3f9
Victor Noël <victor.noel@irit.fr>
2013-04-20 13:31:40 +0200

    Remove old thread pool executor, improves Scheduled (now with asynchronous control) Deprecated some methods in SchedulingControl
    Backported from v2

--- common-components/src/main/java/fr/irit/smac/may/lib/components/scheduling/ScheduledImpl.java
+++ common-components/src/main/java/fr/irit/smac/may/lib/components/scheduling/ScheduledImpl.java
@@ -1,93 +1,143 @@
 package fr.irit.smac.may.lib.components.scheduling;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import fr.irit.smac.may.lib.components.scheduling.interfaces.SchedulingControl;
 import fr.irit.smac.may.lib.interfaces.Do;
 
 public class ScheduledImpl extends Scheduled {
-
-	private final Set<AgentSide> agents = new HashSet<AgentSide>();
 	
-	private final ConcurrentLinkedQueue<AgentSide> newAgents = new ConcurrentLinkedQueue<AgentSide>();
+	// using a concurrent queue because there won't be a lot of producer during execution
+	// (offer relies on a cas mechanism: retries until it works)
+	// and on this class, iterator is ok with concurrent modifications
+	private final ConcurrentLinkedQueue<AgentSide> agents = new ConcurrentLinkedQueue<AgentSide>();
 	
 	public class AgentSide extends Scheduled.Agent {
-
-		private FutureTask<?> currentTask = null;
 		
-		private final AtomicBoolean run = new AtomicBoolean(true);
+		// if this is true, then the agents won't run ever again
+		private final AtomicBoolean killed = new AtomicBoolean(false);
+		
+		// if true, the agent is already in a cycle loop
+		private final AtomicBoolean meRunning = new AtomicBoolean(false);
 		
 		@Override
 		protected void start() {
 			super.start();
-			newAgents.add(AgentSide.this);
+			agents.offer(this);
+
+			if (running.get()) {
+				startRunning(null);
+			}
+		}
+		
+		// this starts a new cycle loop
+		// countdown must be null if running is true
+		public void startRunning(final CountDownLatch countdown) {
+			if (meRunning.compareAndSet(false, true)) {
+				iterativeRun(countdown);
+			} else {
+				if (countdown != null) {
+					countdown.countDown();
+				}
+			}
+		}
+		
+		private void iterativeRun(final CountDownLatch countdown) {
+			sched().executeAfter(new Runnable() {
+				public void run() {
+					if (!killed.get()) {
+						try {
+							cycle().doIt();
+						} catch (Throwable t) {
+							System.err.println("Error when executing cycle in ScheduledImpl:");
+							t.printStackTrace();
+						}
+						if (running.get()) {
+							assert countdown == null;
+							iterativeRun(null);
+						} else {
+							meRunning.set(false);
+						}
+					}
+					if (countdown != null) {
+						countdown.countDown();
+					}
+				}
+			}, sleep);
 		}
 		
 		@Override
 		protected Do make_stop() {
 			return new Do() {
 				public void doIt() {
-					run.set(false);
+					killed.set(true);
+					agents.remove(this);
 				}
 			};
 		}
-		
-		private void tick() {
-			currentTask = new FutureTask<Object>(new Runnable() {
-				public void run() {
-					if (run.get()) {
-						cycle().doIt();
-					}
-				}
-			}, null);
-			sched().execute(currentTask);
-		}
-		
-		private void waitForEnd() {
-			if (this.currentTask != null && !this.currentTask.isCancelled() && !this.currentTask.isDone()) {
+	}
+	
+	private volatile int sleep = 0;
+	
+	// this is true when it is running in cycle loops
+	private final AtomicBoolean running = new AtomicBoolean(false);
+	
+	private void step(boolean synchronous) {
+		if (!running.get()) {
+			sleep = 0;
+			CountDownLatch countdown = null;
+			if (synchronous) {
+				countdown = new CountDownLatch(agents.size());
+			}
+			for(AgentSide a: agents) {
+				a.startRunning(countdown);
+			}
+			if (synchronous) {
 				try {
-					this.currentTask.get();
+					countdown.await();
 				} catch (InterruptedException e) {
 					// TODO what to do here?
-					System.err.println("For an unknown reason, the thread runnig this task has been interrupted");
 					e.printStackTrace();
-				} catch (ExecutionException e) {
-					System.err.println("Error when executing cycle in ScheduledImpl:");
-					e.getCause().printStackTrace();
-				} catch (CancellationException e) {
-					System.err.println("Strangely, the task was canceled but we entered the condition...");
-					e.printStackTrace();
-				} finally {
-					this.currentTask = null;
 				}
 			}
 		}
 	}
-
+	
+	@Override
+	protected SchedulingControl make_async() {
+		
+		return new SchedulingControl() {
+			
+			public void step() {
+				step(false);
+			}
+			
+			public void step(boolean synchronous) {
+				ScheduledImpl.this.step(synchronous);
+			}
+			
+			public void run(final int ms) {
+				sleep = ms;
+				if (running.compareAndSet(false, true)) {
+					for(AgentSide a: agents) {
+						a.startRunning(null);
+					}
+				}
+			}
+			
+			public void pause() {
+				running.set(false);
+			}
+		};
+	}
+	
 	@Override
 	protected Do make_tick() {
 		return new Do() {
 			public void doIt() {
-				while (!newAgents.isEmpty()) {
-					agents.add(newAgents.poll());
-				}
-				// start agents
-				Iterator<AgentSide> it = agents.iterator();
-				while(it.hasNext()) {
-					AgentSide a = it.next();
-					if (a.run.get()) a.tick();
-					else it.remove();
-				}
-				// wait for all of them to finish before giving back control
-				for(AgentSide a: agents) {
-					a.waitForEnd();
-				}
+				step(true);
 			}
 		};
 	}
--- common-components/src/main/java/fr/irit/smac/may/lib/components/scheduling/interfaces/SchedulingControl.java
+++ common-components/src/main/java/fr/irit/smac/may/lib/components/scheduling/interfaces/SchedulingControl.java
@@ -2,10 +2,28 @@ package fr.irit.smac.may.lib.components.scheduling.interfaces;
 
 public interface SchedulingControl {
 	
+	/**
+	 * put the execution of the system on pause
+	 */
 	public void pause();
-	
-	// run every ms milliseconds
+
+	/**
+	 * let the system run indefinitely
+	 * 
+	 * @param ms how long to wait before each cycle
+	 */
 	public void run(int ms);
 	
+	/**
+	 * let the system do one step (all agents run once)
+	 * @param synchronous if true, step will return when the step is finished, else if it will return directly
+	 */
+	public void step(boolean synchronous);
+	
+	/**
+	* prefer to use step(boolean)
+	* this one do it in asynchronous mode
+	*/
+	@Deprecated
 	public void step();
 }
